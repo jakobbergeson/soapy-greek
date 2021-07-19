26a2752f7f80ae1f0b446848a313852e0912eec7
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-07-19 15:54:06 -0700

    created base logic and styles for the navigation bar.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,17 @@ import Layout from "../components/layout"
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/soap.jpeg"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <br></br>
+    <StaticImage
+      src="../images/soap.jpeg"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
