d95e1c0c34c6943d447fc622a66808eade244b51
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-11-08 16:56:40 -0800

    refactored sizing of logo icon.

--- src/icons/cart.jsx
+++ src/icons/cart.jsx
@@ -1,12 +1,12 @@
-import * as React from "react"
+import * as React from "react";
 
 function CartIcon(props) {
   return (
     <svg
-      width={24}
-      height={24}
+      width={40}
+      height={40}
       viewBox="0 0 24 24"
-      fill=" rgb(40, 13, 66)"
+      fill='transparent'
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
@@ -14,15 +14,15 @@ function CartIcon(props) {
       <path
         d="M3.077 5.05A2 2 0 015.076 3h13.848a2 2 0 012 2.05l-.35 14a2 2 0 01-2 1.95H5.426a2 2 0 01-2-1.95l-.35-14z"
         stroke="currentColor"
-        strokeWidth={2}
+        strokeWidth={1.5}
       />
       <path
         d="M16 6c0 2.761-1.79 5-4 5S8 8.761 8 6"
         stroke="currentColor"
-        strokeWidth={2}
+        strokeWidth={1.5}
       />
     </svg>
-  )
+  );
 }
 
-export default CartIcon
+export default CartIcon;
